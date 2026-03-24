4d41046f73ce46f8cde5771c50a61653be818ec4
louvang <vanglou@proton.me>
2026-03-23 22:04:39 -0500

    Add play button and progress bar

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,4 +1,5 @@
 import './Player.css';
+import playIcon from '../assets/icons/play.svg';
 
 interface PlayerProps {
   src: string;
@@ -7,7 +8,19 @@ interface PlayerProps {
 function Player({ src }: PlayerProps) {
   return (
     <>
-      <video src={src} controls className="noteplayer" />
+      <div className="noteplayer-container">
+        <video src={src} className="noteplayer" playsInline />
+        <div className="noteplayer__play-button">
+          <img src={playIcon} alt="play" />
+        </div>
+
+        <div className="noteplayer__progress-container">
+          <div className="noteplayer__progress-bar">
+            <div className="noteplayer__progress"> </div>
+            <div className="noteplayer__timestamp">0:00</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
