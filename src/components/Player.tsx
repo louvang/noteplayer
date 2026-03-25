ef1d9a68edae89cb2263446c0afdc0501f485b36
louvang <vanglou@proton.me>
2026-03-24 22:47:09 -0500

    Implement play/pause functionality

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,17 +1,46 @@
 import './Player.css';
 import playIcon from '../assets/icons/play.svg';
+import pauseIcon from '../assets/icons/pause.svg';
+import { useState, useRef } from 'react';
 
 interface PlayerProps {
   src: string;
 }
 
 function Player({ src }: PlayerProps) {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [playBtnHidden, setPlayBtnHidden] = useState(false);
+
+  const togglePlay = () => {
+    const video = videoRef.current;
+    if (!video) return;
+    setIsPlaying(!isPlaying);
+
+    if (isPlaying) {
+      video.pause();
+      setPlayBtnHidden(false);
+    } else {
+      video.play();
+      setPlayBtnHidden(true);
+    }
+  };
+
   return (
     <>
       <div className="noteplayer-container">
-        <video src={src} className="noteplayer" playsInline />
-        <div className="noteplayer__play-button">
-          <img src={playIcon} alt="play" />
+        <video
+          ref={videoRef}
+          src={src}
+          className="noteplayer"
+          onClick={togglePlay}
+          playsInline
+        />
+        <div
+          className={`noteplayer__play-button ${playBtnHidden ? 'noteplayer__play-button--hidden' : ''}`}
+          onClick={togglePlay}
+        >
+          <img src={isPlaying ? pauseIcon : playIcon} alt="play" />
         </div>
 
         <div className="noteplayer__progress-container">
