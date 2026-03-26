cd3b49d4c998bed5f6afd2f1becb01cd957853bf
louvang <vanglou@proton.me>
2026-03-25 19:30:30 -0500

    Add formatted timestamps

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -11,6 +11,8 @@ function Player({ src }: PlayerProps) {
   const videoRef = useRef<HTMLVideoElement>(null);
   const [isPlaying, setIsPlaying] = useState(false);
   const [playBtnHidden, setPlayBtnHidden] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
 
   const togglePlay = () => {
     const video = videoRef.current;
@@ -26,6 +28,43 @@ function Player({ src }: PlayerProps) {
     }
   };
 
+  const formatTimestamp = (time: number) => {
+    let durationToCalc = time;
+    let hour = 0;
+    let minute = 0;
+    let second = 0;
+    let formattedTime = '';
+
+    if (duration > 3600) {
+      hour = Math.floor(duration / 3600);
+      durationToCalc = duration % 3600;
+      formattedTime = `${hour.toString().padStart(2, '0')}:`;
+    }
+
+    if (durationToCalc > 60) {
+      minute = Math.floor(durationToCalc / 60);
+      durationToCalc = durationToCalc % 60;
+      formattedTime += `${minute.toString().padStart(2, '0')}:`;
+    } else {
+      formattedTime += '00:';
+    }
+
+    second = Math.floor(durationToCalc);
+    formattedTime += `${second.toString().padStart(2, '0')}`;
+
+    return formattedTime;
+  };
+
+  const updateTimestamp = () => {
+    if (!videoRef.current) return;
+    setCurrentTime(videoRef.current.currentTime);
+  };
+
+  const updateDuration = () => {
+    if (!videoRef.current) return;
+    setDuration(videoRef.current.duration);
+  };
+
   return (
     <>
       <div className="noteplayer-container">
@@ -34,6 +73,8 @@ function Player({ src }: PlayerProps) {
           src={src}
           className="noteplayer"
           onClick={togglePlay}
+          onTimeUpdate={updateTimestamp}
+          onLoadedMetadata={updateDuration}
           playsInline
         />
         <div
@@ -46,7 +87,9 @@ function Player({ src }: PlayerProps) {
         <div className="noteplayer__progress-container">
           <div className="noteplayer__progress-bar">
             <div className="noteplayer__progress"> </div>
-            <div className="noteplayer__timestamp">0:00</div>
+            <div className="noteplayer__timestamp">
+              {formatTimestamp(currentTime)} / {formatTimestamp(duration)}
+            </div>
           </div>
         </div>
       </div>
